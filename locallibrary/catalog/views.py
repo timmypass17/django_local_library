39f97e7d8bc80aff261c17d435df3ead53a37d70
timmypass17 <69086587+timmypass17@users.noreply.github.com>
2025-10-04 16:31:30 -0700

    Added librarian permission and all borrowed list

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -74,3 +74,16 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
             .filter(status__exact='o')  # filter by "on loan" status
             .order_by('due_back')   # order by "due_back"
         )
+    
+class LoanedBooksListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects
+            .filter(status__exact='o')  # filter by "on loan" status
+            .order_by('due_back')   # order by "due_back"
+        )
