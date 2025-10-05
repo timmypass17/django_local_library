2402dc8bfca7bd1b83682645293885a9045e7bf3
timmypass17 <69086587+timmypass17@users.noreply.github.com>
2025-10-05 12:24:50 -0700

    Added create, update, delete author

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,6 +9,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from catalog.forms import RenewBookForm
 from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
 
 def index(request):
     """View function for home page of site."""
@@ -128,3 +131,30 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+# Uses author_form.html (seraches for <model>_form.html)
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    # initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    # Not recommended (potential security issue if more fields added)
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
